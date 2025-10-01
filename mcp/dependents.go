e0bb55697e64b51cfbf005da6572c10a27d822d5
Jason Collins <jason.a.collins@gmail.com>
2025-10-01 11:59:01 -0700

    Improve command prompt.

--- mcp/dependents.go
+++ mcp/dependents.go
@@ -7,11 +7,11 @@ import (
 )
 
 type ListDependentsInput struct {
-	Name string `json:"name" jsonschema:"the name of the package to list dependents for"`
+	Package string `json:"package" jsonschema:"the name of the package to list dependents for"`
 }
 
 type ListDependentsOutput struct {
-	Dependents []string `json:"deps" jsonschema:"the dependents of the input dependency package"`
+	Dependents []string `json:"dependents" jsonschema:"the dependents of the input dependency package"`
 }
 
 func (s *server) ListDependents(ctx context.Context, req *mcp.CallToolRequest, in ListDependentsInput) (*mcp.CallToolResult, ListDependentsOutput, error) {
